0a7fc8a9a793e5c94e334d8f45c3b6ffb666e637
PhenixI <zhanzhanlove.hi@gmail.com>
2016-09-18 17:57:45 +0800

    basic codes
    basic codes

---
+++ speed_of_light.py
@@ -0,0 +1,19 @@
+''' Write Python code to print out how far light travels 
+in centimeters in one nanosecond.  Use the values
+defined below.    
+speed_of_light = 299792458   meters per second
+centimeters = 100            one meter is 100 centimeters
+nanosecond = 1.0/1000000000  one billionth of a second
+'''
+
+speed_of_light = 299792458
+centimeters = 100
+nanosecond = 1.0/1000000000
+
+speed_of_light_pernano = speed_of_light*nanosecond*100
+print (speed_of_light_pernano)
+
+speed_of_light = 299792458.0 
+cycles_per_second = 2700000000.0
+
+print(speed_of_light/cycles_per_second)
---
+++ spirit_age.py
@@ -0,0 +1,7 @@
+
+age = 20
+days_in_year = 365.25
+
+days = age*days_in_year
+
+print(days)
